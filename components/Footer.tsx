5f3eafa4b91fe49bded388f5e01fa8db1191d371
Asia4046 <kirito234875@gmail.com>
2025-03-28 11:04:08 +0530

    aesthetic ahhh changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='px-4 sm:px-6 lg:px-8 bg-[#D3CEDF] dark:bg-background'>
-    <div className='flex items-center justify-center w-[97%] max-w-[1800px] h-15 bg-black/90 dark:bg-[#D3CEDF] rounded-md mx-auto my-2 shadow-md shadow-white text-left'>
+    <footer className='px-4 sm:px-6 lg:px-8 bg-[#D3CEDF] dark:bg-background  '>
+    <div className='flex items-center justify-center w-[97%] max-w-[1800px] h-15 bg-black/90 border-2 border-[#F2D7D9] dark:bg-[#D3CEDF] rounded-md mx-auto my-2 shadow-md shadow-white text-left'>
         <p className='text-white/80 dark:text-black/90 text-center font-bold py-4 px-3'>
         Copyright © 2025 Nilesh@A22 &  The Cbse Community. All Rights Reserved
         </p>
